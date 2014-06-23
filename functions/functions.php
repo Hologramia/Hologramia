c4e4e0664402fbece58e9cb26709633cd9608c76
Adolfo Rodriguez <adolfo@merchlar.com>
2014-06-23 10:59:04 -0400

    mysql pass can be something else

--- functions/functions.php
+++ functions/functions.php
@@ -11,9 +11,12 @@ class DB{
 
 		$con = new mysqli("localhost", "root", "", "hologramia_schema");
 		if ($con->connect_errno) {
-			echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
-			self::$connection = NULL;
-			return self::$connection;
+			$con = new mysqli("localhost", "root", "123", "hologramia_schema");
+			if ($con->connect_errno) {
+				echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+				self::$connection = NULL;
+				return self::$connection;
+			}
 		}
 
 		self::$connection = $con;
